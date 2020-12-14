144f83cf69312dc4f95d4d4cb5a01224dc1b009b
Cormac Mc Manus <S00197425@mail.itsligo.ie>
2020-12-14 17:10:55 +0000

    Interface created, employee class created & employee inheritance classes FullTimeEmployee & PartTimeEmployee created

---
+++ Final_Assesment/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Assesment
+{
+    abstract class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string SurName { get; set; }
+
+        abstract public decimal CalculateMonthlyPay();
+
+        
+    }
+
+    class FullTimeEmployee : Employee
+    {
+        public decimal salary { get; set; }
+
+        public override decimal CalculateMonthlyPay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class PartTimeEmployee : Employee
+    {
+        public decimal HourlyRate { get; set; }
+        public double HoursWorked { get; set; }
+
+        public override decimal CalculateMonthlyPay()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
